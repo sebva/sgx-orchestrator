3dfab3bc71813307fdc5f55fb48a5fa92c89f96c
Rafael Pires <rafael.pires@unine.ch>
2017-10-05 16:13:18 +0200

    a bit of documentation and app modified to get enclave binary filename from command line

--- sgx-app/src/dummy.cpp
+++ sgx-app/src/dummy.cpp
@@ -1,14 +1,38 @@
 
 #ifdef ENABLE_SGX
+#include <unistd.h>
+#include <stdlib.h>
+#include <libgen.h>
 #include <stdio.h>
+#include <enclave_dummy_u.h>
+#include <sgx_initenclave.h>
 extern "C" {
 void ocall_print(const char *str) {
     printf("%s",str);
 }
 }
+
+sgx_enclave_id_t global_eid = 0;
 #endif
 
-int main() {
+int main( int argc, char **argv ) {
+#ifdef ENABLE_SGX
+    if( argc != 2 ) {
+        printf("Usage: %s [enclave_filename.signed.so]\n", argv[0]);
+    }
 
+    /* Changing dir to where the executable is.*/
+    char *ptr = realpath( dirname(argv[0]),NULL );
+
+    if( ptr == NULL ){ perror("Error:"); abort(); }
+    if( chdir(ptr) != 0) abort();
+
+    /* Initialize the enclave */
+    if(initialize_enclave( global_eid,
+                           argv[1],"enclave.dummy.token") < 0) {
+        return -2;
+    }
+    free(ptr);
+#endif
 }
 
