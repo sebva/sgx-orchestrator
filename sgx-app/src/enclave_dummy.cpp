baf451aa120786302312fc65d341cb56830edd27
Rafael Pires <rafael.pires@unine.ch>
2017-10-09 14:01:10 +0200

    Testing SGX capabilities

--- sgx-app/src/enclave_dummy.cpp
+++ sgx-app/src/enclave_dummy.cpp
@@ -1,6 +1,8 @@
 #include <enclave_dummy_t.h>
+//#include <sgx_capable.h>
 
 void ecall_dummy(void) {
-
+    //sgx_device_status_t sgx_device_status;
+    //sgx_cap_enable_device( &sgx_device_status );
 }
 
