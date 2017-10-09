e45c79cb43dd5b078c12640a8278b6cdf0f9035e
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2017-10-09 19:41:38 +0200

    Dummy SGX app: memory size choice

---
+++ sgx-app/dummy-entrypoint.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python2
+import argparse
+import os
+import re
+import subprocess
+
+import time
+
+numbers_regex = re.compile("[0-9]+")
+
+available_sizes = sorted(
+    int(numbers_regex.search(x).group(0)) for x in os.listdir('bin-sgx') if numbers_regex.search(x) is not None
+)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("duration", type=float, help="Time that the SGX program has to run")
+parser.add_argument("pages", type=int, help="Number of EPC pages to allocate (circa +2)", default=1000, nargs="?")
+args = parser.parse_args()
+
+requested_size = args.pages
+closest_size = min(available_sizes, key=lambda x: abs(x - requested_size))
+
+print("Using %d as size closest to %d" % (closest_size, requested_size))
+
+sgx_process = subprocess.Popen(("/entrypoint.sh", "./bin-sgx/dummy", "dummy.so.%d.signed" % closest_size))
+try:
+    time.sleep(args.duration)
+except KeyboardInterrupt:
+    pass
+
+print("Killing SGX process")
+sgx_process.kill()
