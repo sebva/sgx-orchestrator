df8f1100360e01bc3a3eb117b4ae321608c3c4b1
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2017-10-11 11:31:13 +0200

    Make program duration optional
    That can be handled by Kubernetes directly

--- sgx-app/dummy-entrypoint.py
+++ sgx-app/dummy-entrypoint.py
@@ -13,7 +13,7 @@ available_sizes = sorted(
 )
 
 parser = argparse.ArgumentParser()
-parser.add_argument("duration", type=float, help="Time that the SGX program has to run")
+parser.add_argument("--duration", "-d", type=float, help="Time that the SGX program has to run. Infinite when not specified")
 parser.add_argument("pages", type=int, help="Number of EPC pages to allocate (circa +2)", default=1000, nargs="?")
 args = parser.parse_args()
 
@@ -23,10 +23,12 @@ closest_size = min(available_sizes, key=lambda x: abs(x - requested_size))
 print("Using %d as size closest to %d" % (closest_size, requested_size))
 
 sgx_process = subprocess.Popen(("/entrypoint.sh", "./bin-sgx/dummy", "dummy.so.%d.signed" % closest_size))
-try:
-    time.sleep(args.duration)
-except KeyboardInterrupt:
-    pass
+if args.duration is not None:
+  try:
+      time.sleep(args.duration)
+  except KeyboardInterrupt:
+      pass
 
 print("Killing SGX process")
 sgx_process.kill()
+
