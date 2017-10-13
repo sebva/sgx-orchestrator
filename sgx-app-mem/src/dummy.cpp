9be3f93867aa69f978030b2fc9688d1548c6334e
Rafael Pires <rafael.pires@unine.ch>
2017-10-13 13:47:53 +0200

    An enclave that does something memory-wise

--- sgx-app-mem/src/dummy.cpp
+++ sgx-app-mem/src/dummy.cpp
@@ -1,9 +1,10 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #ifdef ENABLE_SGX
 #include <unistd.h>
-#include <stdlib.h>
 #include <libgen.h>
-#include <stdio.h>
 #include <enclave_dummy_u.h>
 #include <sgx_initenclave.h>
 extern "C" {
@@ -13,14 +14,17 @@ void ocall_print(const char *str) {
 }
 
 sgx_enclave_id_t global_eid = 0;
+#else
+extern void ecall_dummy(size_t);
 #endif
 
 int main( int argc, char **argv ) {
-#ifdef ENABLE_SGX
     if( argc != 2 ) {
         printf("Usage: %s [enclave_filename.signed.so]\n", argv[0]);
+        return -1;
     }
 
+#ifdef ENABLE_SGX
     /* Changing dir to where the executable is.*/
     char *ptr = realpath( dirname(argv[0]),NULL );
 
@@ -29,11 +33,29 @@ int main( int argc, char **argv ) {
 
     /* Initialize the enclave */
     if(initialize_enclave( global_eid,
-                           argv[1],"enclave.dummy.token") < 0) {
+                           argv[1],"enclave.dummy-mem.token") < 0) {
         return -2;
     }
-    pause();
     free(ptr);
 #endif
+
+    size_t pages;
+    char *fname = basename( argv[1] );
+    char *p1 = strtok(fname,"-"),
+         *p2 = strtok(NULL,".");
+    if( p2 )
+        pages = atoi( p2 ); 
+    else {
+        printf("Filename must be in the form [whatever]-NUMBEROFPAGES.[whatever]\n");
+        return -2;
+    }
+
+    while(true) {
+#ifdef ENABLE_SGX
+        ecall_dummy( global_eid, pages );
+#else
+        ecall_dummy( pages );
+#endif
+    }
 }
 
--- sgx-app-mem/src/enclave_dummy.cpp
+++ sgx-app-mem/src/enclave_dummy.cpp
@@ -1,6 +1,21 @@
+#ifdef ENABLE_SGX
 #include <enclave_dummy_t.h>
+#include <sgx_trts.h>
+#else
+#include <stdio.h>
+#include <stdlib.h>
+#endif
 
-void ecall_dummy(void) {
-
+void ecall_dummy(size_t pages) {
+    size_t sz = pages*(1<<12);
+    void *mem = malloc(sz);
+#ifdef ENABLE_SGX
+    sgx_read_rand( (unsigned char*) mem, sz );
+#else
+    FILE *f = fopen("/dev/urandom","r");
+    fread(mem, 1, sz, f);
+    fclose(f);
+#endif
+    free(mem);
 }
 
