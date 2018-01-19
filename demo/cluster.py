41c2179a1ed8c83e81622eacb88526cd1948807f
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2018-01-19 18:55:57 +0100

    Demo: deploy jobs

--- demo/cluster.py
+++ demo/cluster.py
@@ -6,32 +6,35 @@ from kubernetes.client.rest import ApiException
 
 
 class Cluster(object):
+    sgx_image = "10.75.0.2:5000/sgx-app-mem:1.2"
+    standard_image = "10.75.0.2:5000/standard-app-mem:1.2"
+
     def __init__(self):
         kubernetes.config.load_kube_config()
         self.api = CoreV1Api()
 
-    def launch_pod(self, pod_name: str, duration: int, requested_pages: int, actual_pages: int, image: str):
+    def launch_pod(self, pod_name: str, scheduler: str, duration: int, limit: float, actual: float, is_sgx: bool):
         resource_requirements = V1ResourceRequirements(
-            limits={"intel.com/sgx": requested_pages},
-            requests={"intel.com/sgx": requested_pages}
+            limits={"intel.com/sgx": int(limit / 4096)},
+            requests={"intel.com/sgx": int(limit / 4096)},
         ) if is_sgx else V1ResourceRequirements(
-            requests={"memory": requested_pages * 4096}
-            # Do not set limits, otherwise Kubernetes will kill the pod if it gets exceeded
+            limits={"memory": limit},
+            requests={"memory": limit},
         )
 
         pod = V1Pod(
             api_version="v1",
             kind="Pod",
             metadata=V1ObjectMeta(
-                name="experiment-%s" % pod_name
+                name=pod_name
             ),
             spec=V1PodSpec(
                 termination_grace_period_seconds=0,
-                scheduler_name=scheduler_name,
+                scheduler_name=scheduler,
                 containers=[V1Container(
                     name="app",
-                    image=image,
-                    args=["-d", str(duration), str(actual_pages)],
+                    image=self.sgx_image if is_sgx else self.standard_image,
+                    args=["-d", str(duration), str(int(actual))],
                     resources=resource_requirements
                 )],
                 restart_policy="OnFailure"
--- demo/demo.py
+++ demo/demo.py
@@ -1,9 +1,46 @@
 #!/usr/bin/env python3
 import argparse
 
+import re
+
+from cluster import Cluster
+
+cluster = None
+args = None
+
+units = {
+    "k": 1e3,
+    "M": 1e6,
+    "G": 1e9,
+}
+
 
 def deploy_pod():
     print("deploy")
+
+    match = re.fullmatch(r"(\d+(\.\d+)?)( ?([kMG]))?", args.memory)
+    if match is None:
+        print("Invalid memory usage")
+        return
+    memory = float(match.group(1))
+    unit = match.group(4)
+    try:
+        memory *= units[unit]
+    except KeyError:
+        pass
+
+    match = re.fullmatch(r"(\d+(\.\d+)?)( ?([kMG]))?", args.limit)
+    if match is None:
+        print("Invalid memory limit")
+        return
+    limit = float(match.group(1))
+    unit = match.group(4)
+    try:
+        limit *= units[unit]
+    except KeyError:
+        pass
+
+    cluster.launch_pod(args.name, args.scheduler, args.duration, limit, memory, args.sgx)
 
 
 def node_status():
@@ -32,10 +69,17 @@ global_metrics = {
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Convenience scripts for scheduler demo")
     subparsers = parser.add_subparsers(dest="command")
+    subparsers.required = True
 
     deploy_subparser = subparsers.add_parser("deploy")
     deploy_subparser.add_argument("name", help="Pod identifier")
     deploy_subparser.add_argument("--node", "-n", help="Node on which to deploy the pod (bypass the scheduler)")
+    deploy_subparser.add_argument("--sgx", action="store_true", help="Job will be SGX")
+    deploy_subparser.add_argument("--memory", "-m", "--epc", "-e", required=True)
+    deploy_subparser.add_argument("--limit", "-l", required=True)
+    deploy_subparser.add_argument("--scheduler", "-s", default="", help="Scheduler to use",
+                                  choices=["spread", "binpack", ""])
+    deploy_subparser.add_argument("--duration", "-d", help="Jobs runs for this much seconds", default=300)
 
     node_status_subparser = subparsers.add_parser("node-status")
     node_status_subparser.add_argument("node")
@@ -46,4 +90,5 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    cluster = Cluster()
     functions[args.command]()
